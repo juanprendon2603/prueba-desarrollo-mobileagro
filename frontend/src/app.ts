80f89f7545ebf0ab3306753fe1bf2bee157d7dda
Juan Pablo Rendón <juanprendon2603@gmail.com>
2026-08-25 00:45:59 -0500

    Configurar frontend para despliegue

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,5 +1,9 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:3000'
+    : 'https://prueba-desarrollo-mobileagro.onrender.com';
 
+    
 import type {
   Cliente,
   Producto,
